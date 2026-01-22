541b2919036de6dbdc25640b5a26319fa2e1377e
Eduardo Lopes <eduardoferreiralopes31@gmail.com>
2026-01-22 10:14:10 -0300

    Atualização

--- LanchesMac/Controllers/CarrinhoCompraController.cs
+++ LanchesMac/Controllers/CarrinhoCompraController.cs
@@ -1,24 +1,55 @@
 ﻿using LanchesMac.Models;
 using LanchesMac.Repositories.Interfaces;
+using LanchesMac.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LanchesMac.Controllers
 {
     public class CarrinhoCompraController : Controller
     {
-
         private readonly IlancheRepository _lancheRepository;
-        private readonly CarrinhoCompra carrinhoCompra;
+        private readonly CarrinhoCompra _carrinhoCompra;
 
-        public CarrinhoCompraController(IlancheRepository lancheRepository, CarrinhoCompra carrinhoCompra)
+        public CarrinhoCompraController(
+            IlancheRepository lancheRepository,
+            CarrinhoCompra carrinhoCompra)
         {
             _lancheRepository = lancheRepository;
-            this.carrinhoCompra = carrinhoCompra;
+            _carrinhoCompra = carrinhoCompra;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var itens = _carrinhoCompra.GetCarrinhoCompraItens();
+
+            var carrinhoCompraVM = new CarrinhoCompraViewModel
+            {
+                CarrinhoCompra = _carrinhoCompra,
+                CarrinhoCompraTotal = _carrinhoCompra.GetCarrinhoCompraTotal()
+            };
+
+            return View(carrinhoCompraVM);
+        }
+
+        public IActionResult AdicionarItemNoCarrinhoCompra(int lancheId)
+        {
+            var lanchesSelecionado = _lancheRepository.Lanches.FirstOrDefault(p => p.LancheId == lancheId);
+            if (lanchesSelecionado != null)
+            {
+                _carrinhoCompra.AdicionarAoCarrinho(lanchesSelecionado);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult RemoverItemDoCarrinho(int lancheId)
+        {
+            var lanchesSelecionado = _lancheRepository.Lanches.FirstOrDefault(p => p.LancheId == lancheId);
+            if (lanchesSelecionado != null)
+            {
+                _carrinhoCompra.RemoveDoCarrinho(lanchesSelecionado);
+            }
+            return RedirectToAction("Index");
         }
     }
 }
+
--- LanchesMac/ViewModel/CarrinhoCompraViewModel.cs
+++ LanchesMac/ViewModel/CarrinhoCompraViewModel.cs
@@ -5,7 +5,6 @@ namespace LanchesMac.ViewModel
     public class CarrinhoCompraViewModel
     {
         public CarrinhoCompra CarrinhoCompra { get; set; }
-
         public decimal CarrinhoCompraTotal { get; set; }
     }
 }
