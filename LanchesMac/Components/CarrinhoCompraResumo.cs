1ad9389389cc0255a2012d77834cfbb69c2ecf0b
Eduardo Lopes <eduardoferreiralopes31@gmail.com>
2026-01-26 15:27:26 -0300

    Atualização #3

---
+++ LanchesMac/Components/CarrinhoCompraResumo.cs
@@ -0,0 +1,29 @@
+﻿using LanchesMac.Models;
+using LanchesMac.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Components
+{
+    public class CarrinhoCompraResumo :ViewComponent
+    {
+        private readonly CarrinhoCompra _carrinhocompra;
+
+        public CarrinhoCompraResumo(CarrinhoCompra carrinhocompra)
+        {
+            _carrinhocompra = carrinhocompra;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var itens = _carrinhocompra.GetCarrinhoCompraItens();
+
+            var carrinhoCompraVM = new CarrinhoCompraViewModel
+            {
+                CarrinhoCompra = _carrinhocompra,
+                CarrinhoCompraTotal = _carrinhocompra.GetCarrinhoCompraTotal()
+            };
+
+            return View(carrinhoCompraVM);
+        }
+    }
+}
