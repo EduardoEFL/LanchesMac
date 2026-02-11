08388e08dbc122992fb2006c6c890480d02d914a
Eduardo Lopes <eduardoferreiralopes31@gmail.com>
2026-02-10 21:09:27 -0300

    Atualização #4

--- LanchesMac/Startup.cs
+++ LanchesMac/Startup.cs
@@ -5,6 +5,7 @@ using LanchesMac.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.Web.CodeGeneration.DotNet;
 using System;
 
 namespace LanchesMac;
@@ -54,6 +55,13 @@ public class Startup
 
         app.UseEndpoints(endpoints =>
         {
+          
+            endpoints.MapControllerRoute(
+                name: "categoriaFiltro",
+                pattern: "Lanche/{ Action}/{categoria?}",
+                defaults: new {controller = "Lanche", Action = "List" });
+
+
             endpoints.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
