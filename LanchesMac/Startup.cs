cb8a315408509619d7ecf57df2ded6b5b79fdabf
Moises Ferreira <mfeva2009@gmail.com>
2026-02-26 18:52:03 -0400

    adiciona opcao para adicionar itens no carrinho compra

--- LanchesMac/Startup.cs
+++ LanchesMac/Startup.cs
@@ -63,13 +63,6 @@ public class Startup
 
         app.UseEndpoints(endpoints =>
         {
-          
-            endpoints.MapControllerRoute(
-                name: "categoriaFiltro",
-                pattern: "Lanche/{ Action}/{categoria?}",
-                defaults: new {controller = "Lanche", Action = "List" });
-
-
             endpoints.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
