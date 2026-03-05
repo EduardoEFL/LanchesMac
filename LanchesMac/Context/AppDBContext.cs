c0b873dae01c251ea5b6873edd9c5fbe8a12ee5d
Eduardo Lopes <eduardoferreiralopes31@gmail.com>
2026-03-05 11:57:57 -0300

    Atualização #8

--- LanchesMac/Context/AppDBContext.cs
+++ LanchesMac/Context/AppDBContext.cs
@@ -1,11 +1,13 @@
 ﻿using LanchesMac.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System;
 
-namespace LanchesMac.context
+namespace LanchesMac.Context
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<IdentityUser>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
